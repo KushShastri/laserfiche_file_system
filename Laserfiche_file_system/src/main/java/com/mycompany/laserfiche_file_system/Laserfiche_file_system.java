19cfafe3d080be8c974e245462b4c6bb0d386f7a
ShaneDR <74668311+ShaneDR@users.noreply.github.com>
2023-04-04 18:49:07 -0400

    Add files via upload

---
+++ Laserfiche_file_system/src/main/java/com/mycompany/laserfiche_file_system/Laserfiche_file_system.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laserfiche_file_system;
+
+/**
+ *
+ * @author Shane
+ */
+public class Laserfiche_file_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
